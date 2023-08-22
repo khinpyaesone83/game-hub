1752c75a5273e363c26e2ec18ef29126d2de44bc
Khin Pyae <khinpyaesone167@gmail.com>
2023-08-22 19:55:18 +0630

    Update game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} padding={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={6} padding={10}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
