fa9da02481e970e0474ae2a10646723da2f03c19
Khin Pyae <khinpyaesone167@gmail.com>
2023-08-29 11:21:01 +0630

    Fetch game details

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,7 @@ import SortSelector from "./components/SortSelector";
 function App() {
   // const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
-  return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-    >
-      <GridItem area={"nav"}>
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area={"aside"} paddingX={5}>
-          <GenreList />
-        </GridItem>
-      </Show>
-      <GridItem area={"main"}>
-        <Box paddingLeft={5}>
-          <GameHeading />
-          <HStack spacing={5}>
-            <PlatformSelector />
-            <SortSelector />
-          </HStack>
-        </Box>
-        <GameGrid />
-      </GridItem>
-    </Grid>
-  );
+  return <></>;
 }
 
 export default App;
