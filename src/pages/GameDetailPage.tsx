573bf7af7e58b517e90f12a26fad7ddfbe6f7a06
Khin Pyae <khinpyaesone167@gmail.com>
2023-09-02 20:41:52 +0630

    Fix navbar

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Spinner } from "@chakra-ui/react";
+import { Box, GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
@@ -25,11 +25,17 @@ const GameDetailPage = () => {
   if (error) throw error;
   return (
     <Box padding={5}>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game?.description_raw || ""}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game?.id || 0} />
-      <GameScreenshots gameId={game?.id || 0} />
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game?.name}</Heading>
+          <ExpandableText>{game?.description_raw || ""}</ExpandableText>
+          <GameAttributes game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game?.id || 0} />
+          <GameScreenshots gameId={game?.id || 0} />
+        </GridItem>
+      </SimpleGrid>
     </Box>
   );
 };
